135e173d64e126c57541a360580209413860529a
rosy <rosyshrestha028@gmail.com>
2022-04-11 15:52:31 +0545

    [cleanup] : Remove unused package

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
 export default async ({ app }: { app: express.Application }) => {
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
-    app.use(cookieParser());
     app.use(cors());
 
     console.log('🤭 Express Initialized');
